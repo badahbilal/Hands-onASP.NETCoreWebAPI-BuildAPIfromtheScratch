386edffbb2fb3e782897a9daf75fe4b0fc30ee41
bbadah <bilal.badah@capgemini.com>
2023-09-06 11:39:47 +0100

    21. STEP 2: Add a New Controller

---
+++ cmsWebApi/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cmsWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        public CoursesController()
+        {
+            
+        }
+    }
+}
